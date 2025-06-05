365d162cf2308a98fa149db2f4e4fd3b6c77a98f
cavalry <63081115+hutengyue@users.noreply.github.com>
2025-06-05 13:20:01 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     cors:true
   });
+  app.setGlobalPrefix('api')
   await app.listen(config.host.port);
 }
 bootstrap();
